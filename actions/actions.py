6da9f9f11ccd0227131fda9b43b1d09499347c48
Delfiald <70693122+Delfiald@users.noreply.github.com>
2021-10-28 23:06:15 +0700

    Update actions.py
    add custom action untuk tanya ipk dan ips

--- actions/actions.py
+++ actions/actions.py
@@ -25,3 +25,29 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+class ActionTanyaIP(Action):
+
+    def name(self) -> Text:
+        return "action_tanya_ips_dan_ipk"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        entities = tracker.latest_message['entities']
+        message = "empty"
+        for e in entities:
+            if e['entity'] == 'ip':
+                message = e['value']
+
+        if message == "ips":
+            dispatcher.utter_message(text="IPS atau Indeks Prestasi Semester yaitu sistem penilaian yang diterapkan oleh kampus dan hasil penjumlahan semua nilai mata kuliah selama satu semester")
+        elif message == "ipk":
+            dispatcher.utter_message(text="IPK atau Indeks Prestasi Kumulatif merupakan IPS kumulatif dari seluruh semester yang telah diikuti mahasiswa")
+        elif message == "ip":
+            dispatcher.utter_message(text="IP atau Indeks Prestasi dibuat sebagai indikator keberhasilan mahasiswa dalam mengikuti kegiatan-kegiatan akademik")
+        else:
+            dispatcher.utter_message(text="Apakah anda ingin bertanya tentang index prestasi?")
+
+        return []
